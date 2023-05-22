d943d47f0f84ddb531b73b7bdc2f2f25d106af8a
Nev Flynn <hello@nevflynn.com>
2023-05-22 15:47:20 +0100

    Test

--- app/inbound-relay/api/route.js
+++ app/inbound-relay/api/route.js
@@ -2,9 +2,8 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   const requestBody = await request.json();
-  console.log('Here are the logs:');
-  console.log(requestBody);
-  return NextResponse.json(request.body);
+  // console.log(requestBody);
+  return NextResponse.json(requestBody);
 }
 
 export async function OPTIONS() {
