cba91600f0a95afc8b94fa31a22d8b79e8951b7e
Nev Flynn <hello@nevflynn.com>
2023-05-22 15:29:29 +0100

    Logs

--- app/inbound-relay/api/route.js
+++ app/inbound-relay/api/route.js
@@ -1,11 +1,12 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request) {
+  console.log('REQQQ', request);
   const requestBody = await request.json();
   console.log(requestBody);
   return NextResponse.json(request.body);
 }
 
-// export async function OPTIONS() {
-//   return new Response(null, { status: 200 });
-// }
+export async function OPTIONS() {
+  return new Response(null, { status: 200 });
+}
