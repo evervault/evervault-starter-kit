bdc6c881e58ee84866dc04c37545acec692ddf16
Nev Flynn <hello@nevflynn.com>
2023-05-22 15:19:01 +0100

    Update options

--- app/inbound-relay/api/route.js
+++ app/inbound-relay/api/route.js
@@ -7,12 +7,5 @@ export async function POST(request) {
 }
 
 export async function OPTIONS() {
-  return new Response('Hello, Next.js!', {
-    status: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-  });
+  return new Response(null, { status: 200 });
 }
