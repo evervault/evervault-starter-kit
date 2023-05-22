93b6730dc401f1f1a83b998af54e74b44a646003
Nev Flynn <hello@nevflynn.com>
2023-05-22 12:05:50 +0100

    CORS

--- app/inbound-relay/api/route.js
+++ app/inbound-relay/api/route.js
@@ -3,5 +3,12 @@ import { NextResponse } from 'next/server';
 export async function POST(request) {
   const requestBody = await request.json();
   console.log(requestBody);
-  return NextResponse.json(request.body);
+  return new Response('Hello, Next.js!', {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
