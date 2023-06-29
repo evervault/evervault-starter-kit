3a0a61a3180d23738b7d685c6588cc627fa96547
Thomas Cullen <thomas@evervault.com>
2023-06-29 09:42:53 +0100

    Fix function name

--- app/functions/api/route.js
+++ app/functions/api/route.js
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
-const Evervault = require('@evervault/sdk');
+import { NextResponse } from "next/server";
+const Evervault = require("@evervault/sdk");
 const evervault = new Evervault(process.env.EVERVAULT_API_KEY);
+
+const FUNCTION_NAME = `${process.env.NEXT_PUBLIC_GITHUB_REPO}-functions-distance-to-new-york`;
 
 export async function POST(request) {
   const requestBody = await request.json();
-  const result = await evervault.run('get-distance-to-new-york', requestBody);
+  const result = await evervault.run(FUNCTION_NAME, requestBody);
   return NextResponse.json(result);
 }
