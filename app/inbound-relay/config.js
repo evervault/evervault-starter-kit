9f94f478260f953d1c57bbe25cb7099d8b25fcde
Thomas Cullen <thomas@evervault.com>
2023-07-12 15:33:10 +0100

    Use NEXT_PUBLIC_VERCEL_TEAM_SLUG env var to construct logs link

--- app/inbound-relay/config.js
+++ app/inbound-relay/config.js
@@ -1,3 +1,3 @@
 export const relayDashbordUrl = `https://app.evervault.com/${process.env.NEXT_PUBLIC_TEAM_ID}/${process.env.NEXT_PUBLIC_APP_ID}/inbound-relay/${process.env.NEXT_PUBLIC_RELAY_ID}`;
 
-export const vercelLogsUrl = `https://vercel.com/evervault/${process.env.NEXT_PUBLIC_VERCEL_PROJECT_NAME}/logs`;
+export const vercelLogsUrl = `https://vercel.com/${process.env.NEXT_PUBLIC_VERCEL_TEAM_SLUG}/${process.env.NEXT_PUBLIC_VERCEL_PROJECT_NAME}/logs`;
