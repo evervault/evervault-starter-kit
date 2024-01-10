d2179b5a27c6ba4a0c345c22654034d91976d6c9
Thomas Cullen <thomas@evervault.com>
2024-01-10 10:05:38 +0000

    Add content security policies

--- next.config.js
+++ next.config.js
@@ -1,24 +1,37 @@
+const csp = require("./csp");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['@evervault/sdk'],
-    esmExternals: 'loose',
+    serverComponentsExternalPackages: ["@evervault/sdk"],
+    esmExternals: "loose",
   },
   async headers() {
     return [
       {
-        source: '/inbound-relay/api',
+        source: "/(.*)",
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
           {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          { key: "Referrer-Policy", value: "no-referrer" },
+          { key: "Content-Security-Policy", value: csp },
+        ],
+      },
+      {
+        source: "/inbound-relay/api",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
           },
           {
-            key: 'Access-Control-Allow-Headers',
+            key: "Access-Control-Allow-Headers",
             value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
